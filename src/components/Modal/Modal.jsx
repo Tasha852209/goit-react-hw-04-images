51d1ea554950ac43324255eccaeefca7b9459790
Tetiana Dub <112940506+Tasha852209@users.noreply.github.com>
2023-10-09 22:31:16 +0200

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,36 +1,27 @@
-import React, { Component } from 'react';
+import { useEffect } from 'react';
 
 import css from './Modal.module.css';
 
-class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
+const Modal = ({ toggleModal, children, onClick }) => {
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
 
-  handleKeyDown = event => {
+  function handleKeyDown(event) {
     if (event.code === 'Escape') {
-      this.props.toggleModal(); // Call the toggleModal method to close the modal
+      toggleModal(); // Call the toggleModal method to close the modal
     }
-  };
-
-  handleBackdropClick = event => {
-    if (event.target === event.currentTarget) {
-      this.props.toggleModal();
-    }
-  };
-  render() {
-    const { children, onClick } = this.props;
-
-    return (
-      <div className={css.overlay} onClick={onClick}>
-        <div className={css.modal}>{children}</div>
-      </div>
-    );
   }
-}
+
+  return (
+    <div className={css.overlay} onClick={onClick}>
+      <div className={css.modal}>{children}</div>
+    </div>
+  );
+};
 
 export default Modal;
